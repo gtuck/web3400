14cf37e7b5ed6790452028ee91766f56199e13b8
Garth Tuck <gtuck@weber.edu>
2024-01-09 11:18:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <?php
 
-function listFolderFiles($dir){
+function listFolderFiles($dir, $rootLength)
+{
     $files = scandir($dir);
 
-    foreach($files as $file){
-        if ($file != '.' && $file != '..'){
-            echo '<li><a href="' . htmlspecialchars($dir . '/' . $file) . '">' . $file . '</a>';
-
-            if (is_dir($dir . '/' . $file)) {
-                echo '<ul>';
-                listFolderFiles($dir . '/' . $file);
-                echo '</ul>';
-            }
-
-            echo '</li>';
+    foreach ($files as $file) {
+        // Skip files or folders starting with a dot
+        if ($file[0] == '.') {
+            continue;
         }
+
+        // Create a relative path
+        $relativePath = substr($dir . '/' . $file, $rootLength);
+
+        echo '<li><a href="' . htmlspecialchars($relativePath) . '">' . $file . '</a>';
+
+        if (is_dir($dir . '/' . $file)) {
+            echo '<ul>';
+            listFolderFiles($dir . '/' . $file, $rootLength);
+            echo '</ul>';
+        }
+
+        echo '</li>';
     }
 }
 
-$rootFolder = '/'; // Replace with the path to your root folder
+// Set rootFolder to the directory of the current script
+$rootFolder = __DIR__;
+
+// Length of the root folder path to subtract for relative paths
+$rootLength = strlen($rootFolder);
 
 echo '<ul>';
-listFolderFiles($rootFolder);
+listFolderFiles($rootFolder, $rootLength);
 echo '</ul>';
-
 ?>
